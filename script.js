60c059505731332255b802c698206c4d46173671
Pacinini <72420688+Pacinini@users.noreply.github.com>
2022-05-07 09:56:49 -0500

    Reogranized the html page and finish basic grouping functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var studentPool = []
+var groups = []
 
 function newEntry() {
     //if the message from the user isn't empty then run 
@@ -32,7 +33,43 @@ function newEntry() {
     }
 }
 
+function createGroups() {
+    const numberOfGroups = document.getElementById("number_field").value
 
+    for(let i = 0; i < numberOfGroups; i++) {
+        groups.push([])
+    }
+
+    let g = 0
+    while (studentPool.length > 0) {
+
+        if (g >= numberOfGroups) {
+            g = 0
+        }
+
+        const index = Math.floor((Math.random() * studentPool.length))
+        const student = studentPool.splice(index, 1)
+        groups[g].push(student[0])
+        g++
+    }
+
+    str = ""
+
+    for (let i = 0; i < groups.length; i++) {
+        str += "Group " + (i + 1) + "<br></br>"
+        for (let j = 0; j < groups[i].length; j++) {
+            if (j + 1 == groups[i].length) {
+                str += groups[i][j]
+            } else {
+                str += groups[i][j] + ",\n"
+            }
+            
+        }
+        str += "<br></br><br></br>"
+    }
+    document.getElementById("content-group").innerHTML = str
+    
+}
 
 
 
